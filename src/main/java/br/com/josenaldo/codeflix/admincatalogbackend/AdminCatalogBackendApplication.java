740a879b6332d34b2492925beee2e4ede8951fd6
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2025-01-18 03:12:09 +0000

    feat: Configure SpringBoot project with JPA connection to database

---
+++ src/main/java/br/com/josenaldo/codeflix/admincatalogbackend/AdminCatalogBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.josenaldo.codeflix.admincatalogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminCatalogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminCatalogBackendApplication.class, args);
+	}
+
+}
