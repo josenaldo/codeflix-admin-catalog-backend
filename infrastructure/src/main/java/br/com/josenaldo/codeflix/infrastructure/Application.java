2ee431752dc48d97eaa6dcfc1d75e06ffbb45823
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2025-02-04 02:30:28 +0000

    build: Configure project environments:
    - dev: for development
    - stag: for staging and tests
    - prod: for production
    
    Signed-off-by: Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>

--- infrastructure/src/main/java/br/com/josenaldo/codeflix/infrastructure/Application.java
+++ infrastructure/src/main/java/br/com/josenaldo/codeflix/infrastructure/Application.java
@@ -4,11 +4,13 @@ package br.com.josenaldo.codeflix.infrastructure;
 import br.com.josenaldo.codeflix.infrastructure.config.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "dev");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
