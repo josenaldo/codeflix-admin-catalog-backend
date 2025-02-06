58b06d45e0e2061e7e51bb34abe1d228efe50121
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2025-02-06 18:58:28 +0000

    build: Change Spring profiles to use yml instead of properties
    Signed-off-by: Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
         exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
     }
     implementation("org.springframework.boot:spring-boot-starter-undertow")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     runtimeOnly("com.mysql:mysql-connector-j")
 }
