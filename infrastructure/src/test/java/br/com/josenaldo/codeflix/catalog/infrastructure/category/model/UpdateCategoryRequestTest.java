a803286a67dba90b843af6aa050d76c61b466598
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2025-08-08 15:16:24 -0300

    test: add unit test for UpdateCategoryRequest JSON unmarshalling
    Added `UpdateCategoryRequestTest` to verify correct JSON unmarshalling for `UpdateCategoryRequest` model. Validated fields `name`, `description`, and `is_active` using `@JacksonTest`.
    
    Signed-off-by: Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>

---
+++ infrastructure/src/test/java/br/com/josenaldo/codeflix/catalog/infrastructure/category/model/UpdateCategoryRequestTest.java
@@ -0,0 +1,41 @@
+package br.com.josenaldo.codeflix.catalog.infrastructure.category.model;
+
+import br.com.josenaldo.codeflix.catalog.annotations.JacksonTest;
+import br.com.josenaldo.codeflix.catalog.infrastructure.category.models.UpdateCategoryRequest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+@JacksonTest
+public class UpdateCategoryRequestTest {
+
+    @Autowired
+    private JacksonTester<UpdateCategoryRequest> json;
+
+    @Test
+    void testUnmarshall() throws Exception {
+        // Arrange - Given
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var json = """
+            {
+                "name": "%s",
+                "description": "%s",
+                "is_active": %s
+            }
+            """.formatted(expectedName, expectedDescription, expectedIsActive);
+
+        // Act - When
+        final var parsed = this.json.parse(json);
+
+        // Assert - Then
+        Assertions.assertThat(parsed)
+                  .hasFieldOrPropertyWithValue("name", expectedName)
+                  .hasFieldOrPropertyWithValue("description", expectedDescription)
+                  .hasFieldOrPropertyWithValue("active", expectedIsActive);
+
+    }
+}
