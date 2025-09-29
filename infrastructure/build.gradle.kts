34ba90b96def8d160fd265be2bf2b0a6e4c25c1e
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2025-09-29 12:39:52 -0300

    chore: configure Liquibase plugin and update dependencies
    Integrated Liquibase Gradle plugin into the project by adding required dependencies and plugin configuration. Adjusted paths in master YAML to ensure compatibility with the new structure. Included `picocli` and other necessary libraries for runtime support.
    
    Signed-off-by: Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -2,6 +2,11 @@ buildscript {
     repositories {
         mavenCentral()
     }
+
+    dependencies {
+        classpath(libs.liquibase.core)
+        classpath(libs.mysql.connector.j)
+    }
 }
 
 plugins {
@@ -10,6 +15,7 @@ plugins {
 
     alias(libs.plugins.spring.boot.plugin)
     alias(libs.plugins.spring.dependency.management.plugin)
+    alias(libs.plugins.liquibase.plugin)
 }
 
 group = "br.com.josenaldo.codeflix.infrastructure"
@@ -48,6 +54,54 @@ dependencies {
     testImplementation(libs.test.containers.junit)
 
     runtimeOnly(libs.mysql.connector.j)
+
+    val liquibaseRuntime: Configuration by configurations
+
+    liquibaseRuntime(libs.mysql.connector.j)
+    liquibaseRuntime(libs.picocli)
+    liquibaseRuntime(libs.spring.boot.starter.data.jpa)
+    liquibaseRuntime(libs.liquibase.core)
+}
+
+val changelogFile = "src/main/resources/config/liquibase/master.yaml"
+val databasePort: Int = 33064
+val databaseName = "codeflix_adm_videos"
+val databaseUser = "root"
+val databasePassword = "root"
+val databaseUrl = "jdbc:mysql://localhost:33064/codeflix_adm_videos"
+val databaseDriver = "com.mysql.cj.jdbc.Driver"
+
+liquibase {
+
+//    activities.register("main") {
+//        this.arguments = mapOf(
+//            "changeLogFile" to changelogFile,
+//            "url" to databaseUrl,
+//            "username" to databaseUser,
+//            "password" to databasePassword,
+//            "classpath" to rootDir.toString(),
+//            "driver" to databaseDriver,
+//            "logLevel" to "info",
+//            "referenceUrl" to (
+//                "hibernate:spring:com.org.choosemysnooze" +
+//                    "?dialect=org.hibernate.dialect.M MySQLDialect" +
+//                    "&hibernate.physical_naming_strategy=org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy" +
+//                    "&hibernate.implicit_naming_strategy=org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy"
+//                )
+//
+//        )
+//    }
+
+    activities.register("main") {
+        arguments = mapOf(
+            "changelogFile" to changelogFile,
+            "url" to databaseUrl,
+            "username" to databaseUser,
+            "password" to databasePassword,
+            "driver" to databaseDriver,
+            "logLevel" to "info"
+        )
+    }
 }
 
 tasks {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,11 @@
 rootProject.name = "admin-catalog-backend"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+
 include("domain")
 include("application")
 include("infrastructure")
